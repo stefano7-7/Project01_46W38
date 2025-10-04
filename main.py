c5b5f98929651556d3885a94411277ade3fa4c31
stefano7-7 <stefanodb82@gmail.com>
2025-10-04 21:47:41 +0200

    added alternative power calc func with vectorization and exec time compared

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+"""
+an array of wind speeds is created covering a range from zero to high winds, typical for onshore in Europe
+an interpolation method for the sub-rated part of the power curve is chosen by the User
+a function for power output is called
+a power curve is printed and also plotted
+a comparison in terms of time of execution (efficincy) is made between two power output functions 
+"""
+
 import wind_utilities as wutil
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # power = [];
-wind_speeds = np.linspace(0,30,1000)
+no_points = 1000
+wind_speeds = np.linspace(0,30,no_points)
 interp_method = input("choose [linear, cubic]: ").strip().lower()
 
 if interp_method not in ("linear", "cubic"):
         raise ValueError("interp_method must be 'linear' or 'cubic'")
 
+# inizialization of the variables 
 power_linear = None
 power_cubic = None
 
-# power with both mnethods
+# calling the function calculating the power 
+# with both interp methods according to User choice
 if interp_method == "linear":
     power_linear = [wutil.power_output(ws, "linear") for ws in wind_speeds]
     print(power_linear)
@@ -21,19 +33,35 @@ else:
     print(power_cubic)
 
 
-# power curve plot
-plt.figure(figsize=(8,5))
-if power_linear is not None:
-    plt.plot(wind_speeds, power_linear, label="Linear interp", linewidth=2)
-if power_cubic is not None:
-    plt.plot(wind_speeds, power_cubic, label="Cubic interp", linewidth=2, linestyle="--")
+## efficiency comparison
+# basic function
+timeStart = time.time()
+power_basicFunction = [wutil.power_output(ws, interp_method) for ws in wind_speeds]
+timeFinish = time.time()
 
-# Abbellimenti
+# print only some values, avoiding 0
+no_points = len(power_basicFunction)
+step = max(1, no_points // 10)         
+print(power_basicFunction[1::step])    
+# basic function execution time
+print(f"base function execution time: {(timeFinish - timeStart):.5f} s")
+
+# supposedly improved function
+timeStart = time.time()
+power_vectorizedFunction = [wutil.power_output_vectorized(ws, interp_method) for ws in wind_speeds]
+timeFinish = time.time()
+print(f"vectorized function execution time: {(timeFinish - timeStart):5f} s")
+
+# power curve plot for sanity check
+plt.figure(figsize=(8,5))
+plt.plot(wind_speeds, power_basicFunction, label="basic func", linewidth=2)
+plt.plot(wind_speeds, power_vectorizedFunction, label="vectoriz func", linewidth=2, linestyle="--")
+
 plt.title("Wind Turbine Power Curve")
 plt.xlabel("Wind Speed [m/s]")
 plt.ylabel("Power Output [MW]")
 plt.legend()
 plt.grid(True)
 
-# Mostriamo il grafico
+# show chart & let User close it manually
 plt.show()
--- wind_utilities.py
+++ wind_utilities.py
@@ -1,9 +1,13 @@
-import numpy as np # ready for future addition
+"""
+function power_output()
+function power_output_vectorized()
+"""
+import numpy as np # ready for future improvements
 def power_output(ws: float,
                  interp_method: str, 
-                 # Cp = 0.4, 
-                 # air_density = 1.225,
-                 # rotor_diameter = 100, 
+                 # Cp = 0.4, # ready for future addition
+                 # air_density = 1.225, # ready for future addition
+                 # rotor_diameter = 100, # ready for future addition
                  rated_P = 15, 
                  cut_in_ws =  3,
                  rated_ws = 11,
@@ -13,7 +17,8 @@ def power_output(ws: float,
     function that calculate the power output
     """
     # interpolation method
-    if interp_method not in ("linear", "cubic"): # the input check shall be in the main script, but just in case also here
+    if interp_method not in ("linear", "cubic"): 
+        # the input check shall be in the main script, but just in case is checked here too
         raise ValueError("interp_method must be 'linear' or 'cubic'")
     elif interp_method == "cubic":
         g = ws**3/rated_ws**3
@@ -24,11 +29,41 @@ def power_output(ws: float,
     # ready for exact power calculation with full formula
     # area = np.pi * rotor_diameter**2 / 4 
 
-    if ws < cut_in_ws or ws > cut_out_ws:
-        return 0.0 # turbine stopped
+    if ws < cut_in_ws or ws >= cut_out_ws:
+        # turbine stopped
+        return 0.0 
     elif ws >= rated_ws and ws < cut_out_ws:
-        return rated_P # rated condition
+        # rated condition
+        return rated_P 
     else:
-        return g * rated_P # subrated condition, power output interpolated
+        # subrated condition, power output interpolated
+        return g * rated_P 
 
     return power
+
+def power_output_vectorized(ws: float,
+                 interp_method: str, 
+                 rated_P = 15, 
+                 cut_in_ws =  3,
+                 rated_ws = 11,
+                 cut_out_ws = 25
+                 ):
+    """
+    function that calculate the power output
+    same as power_output(), but added vectorization and check of exceution time for comparison purposes
+    """
+    #
+    ws_array = np.asarray(ws)
+
+    # interpolation method
+    if interp_method not in ("linear", "cubic"): # the input check shall be in the main script, but just in case also here
+        raise ValueError("interp_method must be 'linear' or 'cubic'")
+    elif interp_method == "cubic":
+        g = ws_array**3/rated_ws**3
+    else:
+        # default is linear interpolation
+        g = (ws_array - cut_in_ws) / (rated_ws - cut_in_ws)
+
+    return np.where((ws_array < cut_in_ws) | (ws_array >= cut_out_ws), 0.0, \
+                     np.where(ws_array < rated_ws, g * rated_P, \
+                        rated_P))
